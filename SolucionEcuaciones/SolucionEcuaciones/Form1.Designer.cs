d778d03219011edf104c87235afc2c0c1eca4793
Brayan Giovanni Delion Martínez <103006766+DelionM@users.noreply.github.com>
2023-02-22 22:34:17 -0600

    Add files via upload

---
+++ SolucionEcuaciones/SolucionEcuaciones/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace SolucionEcuaciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.txtEcu = new System.Windows.Forms.TextBox();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ecuación";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(124, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Resultado";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(104, 118);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 3;
+            // 
+            // txtEcu
+            // 
+            this.txtEcu.Location = new System.Drawing.Point(18, 25);
+            this.txtEcu.Name = "txtEcu";
+            this.txtEcu.Size = new System.Drawing.Size(100, 20);
+            this.txtEcu.TabIndex = 4;
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(178, 25);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 20);
+            this.txtX.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(208, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Valor de x";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(114, 62);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 151);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.txtEcu);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.TextBox txtEcu;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ SolucionEcuaciones/SolucionEcuaciones/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SolucionEcuaciones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String extraer, numeros, letras = "", ecuacion;
+        int x, num;
+        double resultado;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+//----------------------------------------------------------------------------------
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            // String numero = txtEcu.Text;
+            // int digito = 0;
+            // int letrass = 0;
+            //// ObtenerChars();
+            // for (int i = 0; i < numero.Length; i++)
+            // {
+            //     digito = Convert.ToInt32(numero[i]) - 48;
+            //     letrass =(numero[i]);
+            //     MessageBox.Show("número "+digito);
+            //     MessageBox.Show("letra " + letrass);
+            //     numero = string.Empty;
+            //    // letrass = string.Empty;
+
+            // }
+            x = Convert.ToInt32(txtX.Text);
+            numeros = "";
+            letras = "";
+            ecuacion = txtEcu.Text.Trim();
+            for (int i = 0; i < ecuacion.Length; i++)
+            {
+                extraer = ecuacion.Substring(i, 1);
+                switch (extraer)
+                {
+                    case "0":
+                    case "1":
+                    case "2":
+                    case "3":
+                    case "4":
+                    case "5":
+                    case "6":
+                    case "7":
+                    case "8":i
+                    case "9":
+                        numeros = numeros + extraer;
+                        break;
+                    default:
+                        letras = letras + extraer;
+                        break;
+                }
+                MessageBox.Show("número " + numeros);
+                MessageBox.Show("letra " + letras);
+                numeros = string.Empty;
+                letras = string.Empty;
+                //CONVERTIR A INT
+                num = Convert.ToInt32(numeros);
+
+                if (letras == "+")
+                {
+                    resultado = num + x;
+                    MessageBox.Show("ressss " + resultado);
+                }
+
+                else if (letras == "-")
+                {
+                    resultado = num - x;
+                    MessageBox.Show("ressss " + resultado);
+                }
+
+                else if (letras == "*")
+                {
+                    resultado = num * x;
+                    MessageBox.Show("ressss " + resultado);
+                }
+
+                else if (letras == "/")
+                {
+                    resultado = num / x;
+                    MessageBox.Show("ressss " + resultado);
+                }
+            }
+        }
+    }
+}
